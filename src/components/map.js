d9c00846d231396a3c9d0705f1518f2ec7f5aa57
Harsh dalwadi <harshdalwadi500@gmail.com>
2022-05-16 22:05:29 +0530

    trying to remove transpile

--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,9 @@
 import React, { useState,useRef, useEffect } from 'react';
+// import ReactMapGL, { FlyToInterpolator, NavigationControl } from 'react-map-gl'; 
+// eslint import/no-webpack-loader-syntax: off 
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';  
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import mapboxgl from 'mapbox-gl';
 
 const Map = (props) => {
@@ -7,6 +12,7 @@ const Map = (props) => {
   
     console.log(rawData + ' ' + markerIndex);
     mapboxgl.accessToken = 'pk.eyJ1IjoiaGFyc2g0MjQ4IiwiYSI6ImNsMnp2eThnbTBoN3ozY3BzZ3M1dHUxaWwifQ.2fhIt6wg6RiWY0FxlKACVA';
+    
     const mapContainerRef = useRef(null);
     const [mapObj,setmapObj] = useState();
     //let map2;
@@ -21,8 +27,8 @@ const Map = (props) => {
     
         // add navigation control (the +/- zoom buttons)
         map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-        
-            
+        /* eslint import/no-webpack-loader-syntax: off */
+        mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
         setmapObj(map);
         // clean up on unmount
         return () => map.remove();
